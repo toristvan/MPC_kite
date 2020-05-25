3dfacbbc0cf3dfdb56384527251988acab64813f
Tor Kjetsaa <toristvansk@gmail.com>
2020-05-25 09:58:02 +0200

    nothing concrete. attempt to implement equations and implicit euler

--- model.py
+++ model.py
@@ -14,19 +14,19 @@ x = vertcat(theta, phi, psi)
 
 #Control input
 u = MX.sym("u")
-u_new = MX.sym("u_new")
+u_old = MX.sym("u_old")
 
 #Parameters
-E0 = MX.sym("E0")
-vm = MX.sym("vm")
-vA = MX.sym("vA")
-vf = MX.sym("vf")
-voff = MX.sym("voff")
-c = MX.sym("c")
-beta = MX.sym("beta")
-rho = MX.sym("rho")
-L = MX.sym("L")
-A = MX.sym("A")
+E0 = 5 # MX.sym("E0")
+vm = 10 # MX.sym("vm")
+vA = 0.5 # MX.sym("vA")
+vf = 0.1 # MX.sym("vf")
+voff = np.pi # MX.sym("voff")
+c = 0.028 # MX.sym("c")
+beta = 0 # MX.sym("beta")
+rho = 1 # MX.sym("rho")
+l = 500 # MX.sym("l")
+A = 30 # MX.sym("A")
 
 #Equations
 v0 = vm + vA*sin(2*np.pi)
@@ -36,9 +36,9 @@ PD = rho*(v0**2)/2
 TF = PD*A*(cos(theta)**2)*(E + 1)*np.sqrt(E**2 + 1)*(cos(theta)*cos(beta) + sin(theta)*sin(beta)*sin(phi))
 
 #Equations of motion
-thetadot = (va/L)*(cos(psi) - tan(theta)/E)
-phidot = -sin(psi)*va/(L*sin(theta))
-psidot = va*u/L + cos(theta)*phidot
+thetadot = (va/l)*(cos(psi) - tan(theta)/E)
+phidot = -sin(psi)*va/(l*sin(theta))
+psidot = va*u/l + cos(theta)*phidot
 
 
 #xdot= vertcat((va/L)*(cos(psi) - tan(theta)/E), -sin(psi)*va/(L*sin(theta)), va*u/L + cos(theta)*phidot)
@@ -46,11 +46,20 @@ xdot = vertcat(thetadot, phidot, psidot)
 
 #Constrains
 hmin = 100
-h = L*sin(x[0])*cos(x[1])
+h = l*sin(x[0])*cos(x[1])
 h_func = Function('h', [x], [h])
 
-#Cost function
+#Cost function/Objective term
 wF = 1e-4
 wu = 0.5
 
 #cost_func = Function('cost', [TF, u_new, u],[-wF*TF + wu*(u_new - u)**2])
+L = -wF*TF + wu*(u_old - u)**2
+
+
+F = Function('F', [x,u, u_old], [xdot, L])
+#Implicit Euler
+neu = 0.1 #step size
+k1 = F(x + neu*k1, u)
+x = x + neu*k1
+
